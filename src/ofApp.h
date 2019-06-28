865995cf7b8026227a61c139f5804e3277ff5910
Honoka Atsuchi <>
2019-06-28 15:15:45 +0900

    test

---
+++ src/ofApp.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp{
+private:
+    //円の大きさが100
+    int size_circle = 100;
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+    float angle = 0;
+};
